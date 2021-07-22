d38258835f0769939f81c4ef9644e476fdcaff30
Seiger <seiger@meta.ua>
2021-07-22 14:30:21 +0300

    [ADD] EN Translate.

---
+++ assets/modules/seigerlang/lang/en.php
@@ -0,0 +1,19 @@
+<?php
+
+$_lang['slang_title'] = 'Managing site language settings';
+$_lang['slang_description'] = '<b>sLang</b> allows you to translate static texts (pop-up messages, button names) as well as content throughout the site.';
+$_lang["slang_dictionary"] = 'Dictionary';
+$_lang['slang_settings'] = 'Settings';
+$_lang['slang_lang_def'] = 'Default language';
+$_lang['slang_select_lang'] = 'Choose language';
+$_lang['slang_field'] = 'Field';
+$_lang['slang_do_not_empty'] = 'should not be empty';
+$_lang['slang_use_url'] = 'Use in URL';
+$_lang['slang_languages'] = 'List of site languages';
+$_lang['slang_must_contain'] = 'and must contain the default language';
+$_lang['slang_must_contain_lang'] = 'and must contain the Default Language as well as values from the Language List';
+$_lang['slang_lang_front'] = 'List of frontend languages';
+$_lang['slang_example_usage'] = 'Use the construct <b>@lang(\'phrase\')</b> to embed translation phrases into a template. for example <b>@lang(\'Hallo World!\')</b>';
+$_lang['slang_synchronize'] = 'Synchronize';
+$_lang['slang_synchronize_help'] = 'Synchronize translations of templates and site database';
+$_lang['slang_auto_translate'] = 'Auto-translation';
