8cc5cc440667fac9477a1920f02b9f3a044798c4
Serhii Kornelyuk <seiger@meta.ua>
2021-08-06 16:16:04 +0300

    [ADD] Hide default lang from url.

--- assets/modules/seigerlang/plugin/plugin.seigerlang.php
+++ assets/modules/seigerlang/plugin/plugin.seigerlang.php
@@ -61,14 +61,18 @@ if ($e->name == 'OnWebPageInit' || $e->name == 'OnPageNotFound') {
     $identifier = evolutionCMS()->config['error_page'];
     $sLangDefault = evolutionCMS()->config['s_lang_default'];
 
-    if (isset($_GET['q'])) {
-        $url = explode('/', $_GET['q'], 2);
+    if (isset($_SERVER['REQUEST_URI'])) {
+        $url = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'), 2);
         $sLangFront = explode(',', evolutionCMS()->config['s_lang_front']);
 
         if (trim($url[0])) {
+            if ($url[0] == $sLangDefault && evolutionCMS()->config['s_lang_default_show'] != 1) {
+                evolutionCMS()->sendRedirect(str_replace($url[0] . '/', '', $_SERVER['REQUEST_URI']));die;
+            }
+
             if (in_array($url[0], $sLangFront)) {
                 $sLangDefault = $url[0];
-                $_GET['q'] = str_replace($url[0] . '/', '', $_GET['q']);
+                $_SERVER['REQUEST_URI'] = str_replace($url[0] . '/', '', $_SERVER['REQUEST_URI']);
             }
         }
     }
@@ -80,10 +84,10 @@ if ($e->name == 'OnWebPageInit' || $e->name == 'OnPageNotFound') {
         evolutionCMS()->config['base_url'] .= $sLangDefault.'/';
     }
 
-    if (!isset($_GET['q']) || !trim($_GET['q'])) {
+    if (!isset($_SERVER['REQUEST_URI']) || !trim($_SERVER['REQUEST_URI']) || $_SERVER['REQUEST_URI'] == '/') {
         $identifier = evolutionCMS()->config['site_start'];
     } else {
-        $q = trim($_GET['q'], '/');
+        $q = trim($_SERVER['REQUEST_URI'], '/');
         $hash = '_'.md5(serialize($q));
         if (array_key_exists($q, UrlProcessor::getFacadeRoot()->documentListing)) {
             $identifier = UrlProcessor::getFacadeRoot()->documentListing[$q];
