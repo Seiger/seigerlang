3de1b541b139a0f3d4c8b8444d6bfe17a1be717c
Serhii Kornelyuk <seiger@meta.ua>
2022-05-26 11:26:23 +0300

    [FIX] Model.

--- assets/modules/seigerlang/models/sLangContent.php
+++ assets/modules/seigerlang/models/sLangContent.php
@@ -1,6 +1,7 @@
 <?php namespace sLang\Models;
 
 use Illuminate\Database\Eloquent;
+use Illuminate\Support\Facades\DB;
 
 class sLangContent extends Eloquent\Model
 {
@@ -8,19 +9,51 @@ class sLangContent extends Eloquent\Model
     protected $fillable = ['resource', 'lang', 'pagetitle', 'longtitle', 'description', 'introtext', 'content', 'menutitle', 'seotitle', 'seodescription'];
 
     /**
-     * Get the post item with lang
+     * Get the content item with lang and original fields
      *
      * @param $query
      * @param $locale
      * @return mixed
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string  $locale
+     * @param  array  $tvNames
+     * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeOriginalAndLang($query, $locale)
+    public function scopeOriginalAndLang($query, $locale, $tvNames = [])
     {
-        return $this->select('*', 'site_content.pagetitle as pagetitle_orig', 'site_content.longtitle as longtitle_orig')
+        $query->select('*');
+
+        if (count($tvNames)) {
+            foreach ($tvNames as $tvName) {
+                $query->addSelect(
+                    DB::raw("(SELECT value FROM ".DB::getTablePrefix()."site_tmplvar_contentvalues
+                        WHERE ".DB::getTablePrefix()."site_tmplvar_contentvalues.contentid = ".DB::getTablePrefix()."s_lang_content.resource
+                        AND ".DB::getTablePrefix()."site_tmplvar_contentvalues.tmplvarid = (
+                            SELECT ".DB::getTablePrefix()."site_tmplvars.id 
+                            FROM ".DB::getTablePrefix()."site_tmplvars
+                            WHERE ".DB::getTablePrefix()."site_tmplvars.name = '".$tvName."'
+                        ) LIMIT 1) as ".$tvName
+                    )
+                );
+            }
+        }
+
+        return $query->addSelect('site_content.pagetitle as pagetitle_orig', 'site_content.longtitle as longtitle_orig')
             ->addSelect('site_content.description as description_orig', 'site_content.introtext as introtext_orig')
             ->addSelect('site_content.content as content_orig', 'site_content.menutitle as menutitle_orig')
             ->leftJoin('site_content', 's_lang_content.resource', '=', 'site_content.id')
             ->where('lang', '=', $locale);
+    }
+
+    /**
+     * Only active resources
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeActive($query)
+    {
+        return $query->where('published', '1')->where('deleted', '0');
     }
 
     /**
